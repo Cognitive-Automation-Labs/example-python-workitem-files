8f7da2d2955c57198560f24a3c7f5a6fa66a24b1
buntoss@gmail.com <buntoss@gmail.com>
2021-07-27 15:41:07 +1000

    added code to variables file for setting up environment

--- variables.py
+++ variables.py
@@ -1,6 +1,13 @@
 from RPA.Tables import Tables
 from RPA.Robocloud.Items import Items
+import os
+import json
 
 tables = Tables()
 items = Items()
 workitem_filename = "data.csv"
+
+with open("./devdata/env.json") as env_in:
+    env_file = json.load(env_in)
+    for key in env_file:
+        os.environ[key] = env_file[key]
