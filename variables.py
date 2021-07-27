bcac5a5183cd4126839a6738a75814423df4d8bf
buntoss@gmail.com <buntoss@gmail.com>
2021-07-27 15:56:57 +1000

    removed env.json file

--- variables.py
+++ variables.py
@@ -6,8 +6,3 @@ import json
 tables = Tables()
 items = Items()
 workitem_filename = "data.csv"
-
-with open("./devdata/env.json") as env_in:
-    env_file = json.load(env_in)
-    for key in env_file:
-        os.environ[key] = env_file[key]
